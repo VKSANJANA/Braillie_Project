f25b8ff4fa8a34c7bf26f56cb250f01d2204beb7
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-06-01 00:55:00 +0530

    Update blue_tts.py

--- blue_tts.py
+++ blue_tts.py
@@ -1,50 +1,17 @@
+import subprocess
 import pyttsx3
-import bluetooth
-import os
-import socket
 
-target_name = "BT-163"
-target_address = None
-
-nearby_devices = bluetooth.discover_devices()
-
-for bdaddr in nearby_devices:
-    print(bdaddr)
-    if target_name == bluetooth.lookup_name( bdaddr ):
-        target_address = bdaddr
-        break
-
-if target_address is not None:
-    print ("found target bluetooth device with address ", target_address)
-else:
-    print ("could not find target bluetooth device nearby")
-
-# pairs device if not paired
-#os.system(f"bluetoothctl -- pair {target_address}")
-
-service_matches = bluetooth.find_service(address=target_address)
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-
-'''
-# lists available ports for the device
-if len(service_matches) > 0:
-    for service in service_matches:
-        if service["protocol"] == "RFCOMM":
-            print(service["port"])
-            #s.connect((target_address, int(service["port"])))
-'''
-
-s.connect((target_address, 2))
-
-# delay to allow socket to establish connection
-for i in range(500000000):
-    pass
-
-#####################################################################
 
 engine = pyttsx3.init('espeak', True)
+#engine = pyttsx3.init(driverName='flite')
 
-engine.say('I been running through the jungle; I been running with the wolves to get to you')
+command = f'sudo bluetoothctl connect CB:09:40:38:C1:A4'
+subprocess.run(command, shell=True)
+
+voice_id = 'english'
+engine.setProperty('voice', voice_id)
+
+engine.setProperty('rate', 170)
+engine.setProperty('volume', 0.5)
+engine.say('Woah, I can speak')
 engine.runAndWait()
-
-s.close()
