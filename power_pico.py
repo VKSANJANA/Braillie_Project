2268c347fb614268273e17dbf449bfb1cc896fff
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-06-05 18:32:56 +0530

    Update power_pico.py

--- power_pico.py
+++ power_pico.py
@@ -1,11 +1,12 @@
 from machine import Pin
-from time import sleep
+from time import time
+
 
 power = Pin(21, Pin.IN)
 pi_zero = Pin(20, Pin.OUT)
-switch = Pin(22, Pin.OUT)
+fet = Pin(22, Pin.OUT)
 
-switch.on()
+fet.on()
 pi_zero.on()
 
 status = 0
@@ -14,14 +15,17 @@ def power_control():
     global status
     if power.value():
         if status:
-            switch.on()
+            fet.on()
             pi_zero.on()
             status = 0
     else:
-        pi_zero.off()
-        sleep(15)
-        switch.off()
-        status = 1
+        if not status:
+            pi_zero.off()
+            time_then = time()
+            while (time() - time_then) < 90:
+                pass
+            fet.off()
+            status = 1
 
 while True:
     power_control()
