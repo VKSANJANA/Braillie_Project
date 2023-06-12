9e6942f1496f483c494b84f8577431e3d448a5fd
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-06-12 14:21:11 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,27 +1,17 @@
 # Import the python text to speech libarary and the PDF REader library
 
 import pyttsx3 
-
 from PyPDF2 import PdfReader
-
-from blu_os import *
+import blu_os as blu
 
 # Read the PDF file binary mode
 #Pass file code to be added
+a = blu.audio()
 
 with open(file_path,"rb") as f:
-
 	pdf=PdfReader(f)
-
 	num_of_pages=len(pdf.pages)  #find the number of pages
-
 	for i in range(num_of_pages):
-
 		page=pdf.pages[i]
-
 		page_content=page.extract_text()
-
 		a.say(page_content)
-
-		
-
