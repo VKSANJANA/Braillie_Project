10f52e2e40240f71042762de25d294f22b5a87f8
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2023-07-07 00:07:04 +0530

    Update hmi.py

--- 14Jun/hmi.py
+++ 14Jun/hmi.py
@@ -13,6 +13,8 @@ class HMI():
 	down = 3
 	ok = 6
 	back = 7
+	trigger = 'n'
+	long_press = True
 
 	def __init__(self):
 		pcf_address = 0x20
@@ -40,7 +42,19 @@ class HMI():
 	                if not key_status[i+2]:
 	                        print("Button "+str(i)+" pressed")
         	                self.key_pressed = key_status[+2]
-
+				
+	def long_press(self, pin):
+		self.stat = read_input()
+		if not stat[self.ok] or not stat[self.back]:
+			time_start = time.time()
+			while time.time() - time_start < 5:
+				pass
+			if not read_input()[self.ok]:
+				self.trigger = 'r'
+			elif not read_input()[self.back]:
+				self.trigger = 'a'
+			else:
+				self.long_press = False
 '''
 hmi = HMI()
 GPIO.add_event_detect(13, GPIO.FALLING, callback=hmi.isr_hmi, bouncetime=2)
@@ -50,4 +64,27 @@ try:
 		pass
 except KeyboardInterrupt:
 	GPIO.cleanup()
+'''
+'''
+hmi = HMI()
+GPIO.add_event_detect(13, GPIO.FALLING, callback=hmi.isr_hmi, bouncetime=2)
+
+ok = True 
+back = True
+try:
+	print('press okay to continue and back to exit')
+	while ok and back:
+		if not hmi.long_press:
+			ok = hmi.stat[hmi.ok]
+			back = hmi.stat[hmi.back]
+			hmi.long_press = True
+		else:
+			if hmi.trigger == 'r':
+				pass # read time
+				hmi.trigger = 'n'
+			elif hmi.trigger == 'a':
+				pass # read adc
+				hmi.trigger = 'n'
+			else:
+				pass
 '''
